198fd427e42bd399def5761cf74f2aa3045aff9b
karelbondan <karelbondan@gmail.com>
2024-08-06 14:04:46 +0700

    Added icons to remove dullness; Adjusted footer (navbar) size; Changed from useRouter to Link to allow next-toploader to show loading bar; Added conditional render to disable navbar on login page

--- components/cards/time/index.tsx
+++ components/cards/time/index.tsx
@@ -19,7 +19,10 @@ export default function TimeCard() {
     }, []);
 
     return (
-        <div className="rounded-lg bg-blue-200 p-3">
+        <div className="flex space-x-1 rounded-lg bg-blue-200 p-3">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
             <p>{currentDate.toDateString()}, {currentTime}</p>
         </div>
     )
--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,19 +1,37 @@
 'use client'
-import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function NavigationBar() {
-    const router = useRouter();
+    const pathname = usePathname();
     return (
-        <div className="border grid grid-cols-3">
-            <button onClick={() => router.push('/')} className="flex items-center justify-center border p-5 active:scale-95 active:bg-black/15 transition-all">
-                Dashboard
-            </button>
-            <button onClick={() => router.push('/data_harian')} className="flex items-center justify-center border p-5 active:scale-95 active:bg-black/15 transition-all">
-                Data harian
-            </button>
-            <button onClick={() => router.push('/grafik')} className="flex items-center justify-center border p-5 active:scale-95 active:bg-black/15 transition-all">
-                Grafik
-            </button>
-        </div>
+        !pathname.startsWith("/login") ? (
+            <div className="border grid grid-cols-3 text-sm">
+                <Link href={"/"} className="flex flex-col items-center justify-center border px-5 py-2 active:scale-95 active:bg-black/15 transition-all">
+                    <button className="flex flex-col items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                        </svg>
+                        <p>Dashboard</p>
+                    </button>
+                </Link>
+                <Link href={"/data_harian"} className="flex flex-col items-center justify-center border px-5 py-2 active:scale-95 active:bg-black/15 transition-all">
+                    <button className="flex flex-col items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+                        </svg>
+                        <p>Data harian</p>
+                    </button>
+                </Link>
+                <Link href={"/grafik"} className="flex flex-col items-center justify-center border px-5 py-2 active:scale-95 active:bg-black/15 transition-all">
+                    <button className="flex flex-col items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z" />
+                        </svg>
+                        <p>Grafik</p>
+                    </button>
+                </Link>
+            </div>
+        ) : null
     )
 }
