f0dee34cd16494395990b6ba2d0d2802c03101c7
karelbondan <karelbondan@gmail.com>
2024-08-06 14:11:50 +0700

    Added back button; Adjusted path display formatting to accommodate smaller screens; Added conditional rendering to disable this component in login page

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,15 +1,34 @@
 'use client'
-import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
+
+function formatPathname(pathname: string) {
+  const splitted = pathname.split("/");
+  console.log(splitted);
+  if (splitted.length > 3)
+    return `${splitted[1]} - ... - ${splitted[splitted.length - 1]}`.replaceAll("_", " ")
+  else
+    return pathname.replace("/", "").replaceAll("_", " ").replaceAll("/", " - ");
+}
 
 export default function Header() {
   const pathName = usePathname();
-  const formattedPathName = pathName.replace("/", "").replace("_", " ");
+  const router = useRouter();
+  const formattedPathName = formatPathname(pathName);
+
   return (
-    <div className="border w-full justify-between flex p-6">
-      <Link href={"/"}>
-        <h1 className="font-bold text-2xl">{formattedPathName == "" ? "Dashboard" : formattedPathName.split(" ").map(word => word[0].toUpperCase() + word.slice(1)).join(" ")}</h1>
-      </Link>
-    </div>
+    !pathName.startsWith("/login") ? (
+      <div className={`border w-full flex items-center px-6 ${formattedPathName == "" ? "space-x-0 py-6" : "space-x-5 py-5"}`}>
+        <button
+          className={`bg-gray-300 size-10 min-w-10 min-h-10 rounded-lg items-center justify-center ${formattedPathName == "" ? "hidden" : "flex"} active:scale-75 transition-all`}
+          onClick={() => router.back()}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+          </svg>
+        </button>
+        <h1 className="font-bold text-2xl">{formattedPathName == "" ? "Dashboard - Mreneo" : formattedPathName.split(" ").map(word => word[0]?.toUpperCase() + word.slice(1)).join(" ")}</h1>
+      </div>
+    ) : null
   )
 }
